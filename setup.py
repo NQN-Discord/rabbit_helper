ca61d131305fe83cc9953ba13decb414814a59db
Blue <redacted>
2020-06-07 14:32:15 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+   name="rabbit_helper",
+   version="0.1.0",
+   description="A helper for RabbitMQ which allows for protocol versioning",
+   author='Blue',
+   url="https://nqn.blue/",
+   packages=["rabbit_helper"],
+   install_requires=["aio-pika"]
+)
